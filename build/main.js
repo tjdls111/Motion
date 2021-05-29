62a1d05d98016592ffc61939537799812516504a
tjdls111 <tjdls111@naver.com>
2021-05-29 17:47:58 +0900

    youtube 추가

--- build/main.js
+++ build/main.js
@@ -12,10 +12,27 @@
         Img.prototype.make = function (url) {
             var img = document.createElement('img');
             img.setAttribute('src', url);
-            img.setAttribute;
+            img.setAttribute('class', 'content');
             main_1 === null || main_1 === void 0 ? void 0 : main_1.appendChild(img);
         };
         return Img;
     }());
-    var img = new Img('good img', 'https://picsum.photos/300/300', 'good');
+    var Video = /** @class */ (function () {
+        function Video(title, url, body) {
+            this.title = title;
+            this.url = url;
+            this.body = body;
+            this.make(url);
+        }
+        Video.prototype.make = function (url) {
+            var video = document.createElement('iframe');
+            video.innerHTML = '<iframe width="560" height="315" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+            video.setAttribute('src', "https://www.youtube.com/embed/ov3NRyoIEQ4");
+            video.setAttribute('class', 'content');
+            main_1 === null || main_1 === void 0 ? void 0 : main_1.appendChild(video);
+        };
+        return Video;
+    }());
+    var img = new Img('good img', 'https://picsum.photos/300/200', 'good');
+    var vid = new Video('fun', 'fdf', 'seventeen');
 }
--- src/main.ts
+++ src/main.ts
@@ -24,15 +24,30 @@
         make(url:string){
             const img = document.createElement('img');
             img.setAttribute('src',url);
-            img.setAttribute
+            img.setAttribute('class','content')
             main?.appendChild(img);
-        }
-
-       
+        }  
     }
 
-    const img=new Img('good img','https://picsum.photos/300/300','good');
- 
+    class Video implements ContentMaker{
+        constructor(readonly title:string,readonly url:string,readonly body:string){
+            this.make(url);
+        }
+    
+        make(url:string){
+            const video = document.createElement('iframe');
+            video.innerHTML='<iframe width="560" height="315" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+            video.setAttribute('src',"https://www.youtube.com/embed/ov3NRyoIEQ4");
+            video.setAttribute('class','content')
+            main?.appendChild(video);
+        }  
+    }
+
+
+
+
+    const img=new Img('good img','https://picsum.photos/300/200','good');
+    const vid=new Video('fun','fdf','seventeen');
     
     
     
